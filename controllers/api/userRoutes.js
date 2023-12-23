106803deeb0bd3532fe48a9aab3ffa2160bfb909
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-23 18:23:31 -0500

    user able to logout, redirected to login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -37,5 +37,15 @@ router.get('/', (req, res) => {
       res.status(400).json(err);
     }
   });
+
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
   
   module.exports = router;
