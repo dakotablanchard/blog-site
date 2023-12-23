73f84950a2a08ba56a81e3d964868e1b8a425702
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-23 18:15:51 -0500

    user able to log in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,22 +2,23 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/home', async (req, res) => {
+router.get('/home', withAuth, async (req, res) => {
   try {
-        // Get all projects and JOIN with user data
-        const postData = await Post.findAll({
-          include: [
-            {
-              model: User,
-              attributes: ['name'],
-            },
-          ],
-        });
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-        const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     res.render('homepage', {
       posts,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
