a301df1f765e5ac6656aa0ee3d543063f84f39bf
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-12-31 17:38:37 -0500

    fixed bug that set default character length to 255 in models Post, and Comment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# blog-site
+# Blog Site
 
 ## Credits
 
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,6 +3,7 @@ const { Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/create-post', async (req, res) => {
+  console.log(req.body.title, req.body.content, req.session.user_id)
   try {
     const newPost = await Post.create({
       title: req.body.title,
